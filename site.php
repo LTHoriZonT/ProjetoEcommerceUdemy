bd3d9da0a99d2c2f41378aa48ebdd29669f981b2
Isaque Mendes <isaquemendes405@gmail.com>
2021-02-04 12:55:26 -0300

    Site-Produtos

--- site.php
+++ site.php
@@ -4,9 +4,13 @@ use \Hcode\Page;
 
 $app->get('/', function() {
     
+	$products = Product::listAll();
+
 	$page = new Page();
 
-	$page->setTpl("index");
+	$page->setTpl("index", [
+		"products"=>Product::checkList($products)
+	]);
 
 });
 
