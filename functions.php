29b55dae78811f8eacea0fc4e5c792c3f6819112
Isaque Mendes <isaquemendes405@gmail.com>
2021-02-07 20:07:53 -0300

    Meus pedidos e resumo carrinho

--- functions.php
+++ functions.php
@@ -35,4 +35,27 @@ function getUserName()
 
 }
 
+function getCartNrQtd()
+{
+
+	$cart = Cart::getFromSession();
+
+	$totals = $cart->getProductsTotals();
+
+	return $totals['nrqtd'];
+
+}
+
+function getCartVlSubTotal()
+{
+
+	$cart = Cart::getFromSession();
+
+	$totals = $cart->getProductsTotals();
+
+	return formatPrice($totals['vlprice']);
+
+}
+
+
  ?>
